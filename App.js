7a610f752b1d77582d793eaa660e7178d39b9b4c
yokoref <yokoref@gmail.com>
2017-10-15 18:41:54 -0500

    2nd commit using more up to date code

--- App.js
+++ App.js
@@ -9,30 +9,27 @@ import {
   Platform,
   StyleSheet,
   Text,
-  View
+  View,
+  AppRegistry
 } from 'react-native';
 
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' +
-    'Cmd+D or shake for dev menu',
-  android: 'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+
+import Buttons from './app/Buttons';
+import ResultDisplay from './app/ResultDisplay';
+
+
 
 export default class App extends Component<{}> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>
-          Welcome to React Native!
-        </Text>
-        <Text style={styles.instructions}>
-          To get started, edit App.js
-        </Text>
-        <Text style={styles.instructions}>
-          {instructions}
-        </Text>
+        <Text>Hello you hunka hunka!</Text>
+       <ResultDisplay/>
+        <Buttons/>
+
+
       </View>
+
     );
   }
 }
@@ -40,18 +37,8 @@ export default class App extends Component<{}> {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
-  welcome: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-  },
-  instructions: {
-    textAlign: 'center',
-    color: '#333333',
-    marginBottom: 5,
+    flexDirection: 'column',
   },
 });
+
+AppRegistry.registerComponent('App', () => App);
